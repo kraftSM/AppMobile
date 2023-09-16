6cd76917ade3e82df34301301a2c31093ae97032
kraftsm <kraftsm@yandex.ru>
2023-09-16 12:46:22 +0300

    Добавьте файлы проекта.

---
+++ HomeApp/HomeApp/LoadingPage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeApp
+{
+    class LoadingPage : ContentPage
+    {
+        public LoadingPage()
+        {
+            // Объявим новый текстовый элемент
+            Label header = new Label() { Text = $"Запуск моего 1-го приложения{Environment.NewLine} на Xamarin... для Гришки!!!! " };
+
+            // Здесь можно сразу установить стили и шрифт
+            header.Opacity = 0;
+            header.HorizontalTextAlignment = TextAlignment.Center;
+            header.VerticalTextAlignment = TextAlignment.Center;
+            header.FontSize = 21;
+            // Можем даже задать анимацию
+            header.FadeTo(1, 3000);
+
+            // Инициализация свойства Content новым элементом.
+            this.Content = header;
+        }
+    }
+}
---
+++ HomeApp/HomeApp/SpanPage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace HomeApp
+{
+    public class SpanPage : ContentPage
+    {
+        public SpanPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label 
+                    { 
+                        Text = "Welcome to Xamarin.Forms!" 
+                    }
+                }
+            };
+        }
+    }
+}
